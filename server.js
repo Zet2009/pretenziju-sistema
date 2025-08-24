44074e3dffffd1548eb3365d3bdf41f4aa9ad142
Zet2009 <kozlovskizenon@gmail.com>
2025-08-24 17:05:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
